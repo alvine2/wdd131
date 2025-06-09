c9cf6c2498240bb2665d4b5daa27df4c5d42953a
alvine2 <alvinekinyera@gmail.com>
2025-06-09 12:02:03 +0300

    page updated

--- scripts/review.js
+++ scripts/review.js
@@ -1,27 +1,27 @@
 const products = [
     {
         id: "fc-1888",
-        name: "flux capacitor",
+        name: "React website",
         averagerating: 4.5
     },
     {
-        id: "fc-2050",
-        name: "power laces",
+        id: "Games",
+        name: "popular games",
         averagerating: 4.7
     },
     {
-        id: "fs-1987",
-        name: "time circuits",
+        id: "React + Tailwind.css+Next.js",
+        name: "latest tech stack",
         averagerating: 3.5
     },
     {
-        id: "ac-2000",
-        name: "low voltage reactor",
+        id: "Python code",
+        name: "server side rendering",
         averagerating: 3.9
     },
     {
-        id: "jj-1969",
-        name: "warp equalizer",
+        id: "CSharp",
+        name: "backend costs",
         averagerating: 5.0
     }
 ];
